f4d2f1c4058747a725ef56e18132ca60b00cd92b
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 19:57:50 -0400

    Create 3_spider_man.cpp

---
+++ Obfuscated_code/3_spider_man.cpp
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#define spiderman main
+#define ned int
+//#define mysterio clrscr
+#define doctor_strange for
+#define MJ printf
+ned spiderman(){/*mysterio();*/ned c,m,j;doctor_strange(ned
+i=((4<<0)+(1<<5) +4-(2<<10)+16+2+2022+( 2<< 0)-(2<<4)-2*01),
+m=(1<<6)+(1<<6 )+2 +9+(2<<10)-17*2-1001 +0 +(1<<5)-(1<<7);c=
+"KEY=4+20=024"  "" "4spd=OCTPOSTALLIVEN" "=" "A&()C++;A&()P"
+"++;=OCTOPUSA" "A" "++C();A&()TOM+ANT=B"  ""  ";SHANG;PI()="
+"+=WONG+ANT;"  ""  "CharAt(-TOM+ANT+CAP)" "A" "++TOBEY=RAIN"
+"BADBLADBLAD" "A"  "!UBUNTUAI()CARNAGE;O"  ""  "TOM=-WANDA;"
+"ABC=CBA;ADD" "P"  "UNISBOOMBOOMBOOMNEXT"  ""  "=BOOMNOOB()"
+"(B++)B&=-V"  ""  "CPTAIN;CCCCAPTAN()+=&"  "A" "(MAYHAPPYC)"
+"NOBODYVEIN"  ""  "BOOMBOOMBOOOMMM+=BOOM"  "A"  "BLAUNTCARL"
+"BMARVEL=B;"  ""  "~ABNORBMALMNAT=-HULK;"   ""  "=BLIPBOOMB"
+"DR.WANGBY"  "A"  "ACTORA" "BOOM" "T()=T"   ""  "++LOKIBLIP"
+"BOOMBLOWW"  ""   "RUGBY"  "LUCY"  "=(B)"   ""  "++LOKI=&L;"
+"LOKIBITEB"  "A"          "" "" ""          ""   "ROBAT=WIN"
+"MCU=!BLIP"  "BY"  "SALT"          "===="  "="   "=KILLBILL"
+"CLANBLIP"   "HIDRA"                    "LEAF"   "MARAPAGGY"
+"=MCU()B;"   "A"                           "="   "B;FALCON="
+"HANKPYMB"         "NEWB"         "SLACK"        "HOGAN(B);"
+"!FREEBALL"     "F1"                   "ROC"    "BOBBOBOBOB"
+"ALICE=BOOK"  "NEWB"                    "LOOC" "NEDJACOBATA"
+"ALONBNEDAS+=B();"   "J"            ""   "NEWBBLACKJAKSON=P"
+"NAKIA-BLOOD()-B"   "A"              ""   "++FURYJR()FOODBY"
+"ALCANECNICKFUR"   ""   ""        ""   ""  "=DONJ=++C=MCGIL"
+"INDIAANDREW()"   ""    ""        ""   "F"   "FALCON=KONG=1"
+"erJsldCoolCl"  "LL"   "a"        ""   "FO"   "iCoolCOOLK++"
+"B=BIGJARVI"   "ILL"   "F"        ""   "FOL"   "IRONGUN+HIT"
+"!KILL=MAY"   "KILL"   "E"        ""   "FOLD;"   "JBSONIEND"
+"!DRAXL"     "UCK=!"   "D"       "J"   "LOOPBY"    "A=FIGHT"
+"LLOFTT"   "MALCOM;"   "E"       "A"   "=IMARTI"   "NCLUBC)"
+"FALCON"   "BBLACKK"   ";S"      "Y"   "++=--(Q)"  "=-CROOC"
+"MMMJJJ"   "RANKICI"   "LS"      "B"   "FLICHERR"  "111=111"
+"PoiuCd"   "ROBERTC"   ";B"     "AB"   "!=THOR;A"  "DARDEVI"
+"Bja5B$f"  "AQUMA=N"   "PRO"    "JAY"  "ClubBoNd"  "DOORBEL"
+"AHSJLBW"  "ROCKLOC"   "PRO"    "CNN"  "LOKI!=LA"  "INDIABY"
+"INDIA=C"  "FALCON;"   "HELA"  "BOOM"  "ClubDON"   "iClowN;"
+"@R=REAL"  "C=!CLUEE"  "FALCON=COOLH"  "!ALBERT"   "CjklEi;"
+"1C111M1"  "BA=ROCK;"  "klClifFlicC;"  "alChemH"  "CLUB=23C"
+"KORG++;"   "CoOkKar"  "P=ronHULkELF"  "AGENTCA"  "RTCPEGGY"
+"!ICE()CI"  "5#H()*D"  "+IWINTE=R()"   "HULKCLN"  "HI-TECH:"
+"MCUyeG()"  "iCU=MCu"  "flICkeCooLI"   "ABC=XYZ"  "HTEACHER"
+"IS!=COOL"  "D=DEFEN"  "aCfweCfnhJa"  "ROBERTIS" ">>=RICHER"
+"BCDFRIDAY" "COLDD()"  "@FALCCON=C;"  "JJJJAYBB  IS;I=NAKIA"
+"NEBULAIS;" "!CLEVER;" "!DARDEVILBY"  "JJJJAYBB  IS;I=*ODIN"
+"NEBULAI()"  "!CLEVER" "++DARDEVILB"  "JJJJAYBB @WITCH=SHUI"
+"NNEBULAI()" "!CLIFF;"  "=EBONYFAIL"  "BENDIN;" "ACATSI()+="
+"ANCIENTBOB" "AJAX=B;"  "++NICKBOND" "IFALCON" "CIVIL=WARIS"
+"NEBULAJAR;B" "*i-N++"  "ABYSSISDUB" "MMMJJJ!= SPIDERCITYIN"
+"NEBULANOT=B" "&SPIDER" "BRK=JUMP=M  JANEBINGIS!-B;GAMORAC1"
+"NEILNEBULAJEAN=D0NJ()" "(B)FRIDAYB  !MARYB() JANEAS!=KURG="
+"DARKLOGANISINROBERTM;"  "ROBERTJR; A111K111=111K111A111J11"
+"FARFRMORBBIUSISNEBULAK; A=-LOKI()B QUICK=ANT-MAN;BVISION;A"
+"<>BLACK=-SILVERADAMI()" "iBALLL() BANCIENTASFALCON=iFALCON"
+"ALDRICH=BELELIZAABETI() ADORMAMU= EMMA()ACIPHER=BIN-LOSTAI"
+"!WITCHADORMAMU=DORMAMUAS TITANATA KILLBILLWONG=DR.WONGAPI;"
+"KARPBY!=HELAAODINSON=!HAWKEYBONDJLOGANMRX()+=MAXX!DRAX-UJI"
+"++APROXIMA=DRAX++;AISPIDERALLYY()!=machinHANKPYM()=FURY();"
+"ANT=hulk"[(!(c-61)?(m=1<<5|1,MJ("\n")):(m^=1)),i+=(3+1)];){
+doctor_strange(j=0;(c-2<<5+3)&&(j++-c-(c<(2<<5)?(2<<1+1):(c>
+((2<<5)+(2<<4)-(2<<1)-2)?-((2<<5)+(2<<1)+(2)):-(2<<5))));MJ(
+"%c",m));}return (2<<1)+(2<<5)+(2<<4)+(2<<2)+(2<<3)-(2<<6);}
+/**********************************************************/
+/*                 SPIDERMAN : NO WAY CODE                */
+/*                           BY                           */
+/*                  PROGRAMMING KNOWLEDGE                 */
+/**********************************************************/
