b476b5fb17f0c3f1879a75d490a4529cd35b55cf
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 18:24:31 -0400

    Create 5_bol_do_na_zara_bollywood.cpp

---
+++ music tunes in C language/5_bol_do_na_zara_bollywood.cpp
@@ -0,0 +1,85 @@
+/* MADE BY:- PROGRAMMING KNOWLEDGE
+ * FOR MORE VIDEOS SUBSCRIBE MY CHANNEL : https://www.youtube.com/c/programmingknowledge001/videos
+ * LIKE SHARE AND SUBSCRIBE
+ */
+
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+void main()
+{
+clrscr();
+int a=370,b=555,c=740,d=330,e=390,f=440,g=490,h=590;
+int i=660,k=880,j=0;
+printf("Bol Do Na Zara music tune by Programming Knowledge \n Lyrics : \n");
+printf("\n Itni mohabbat karo na\n Main doob na jaaun kahin\n Waapas kinaare pe aana\n Main bhool na jaaun kahin\n Dekha jab se hain chehra tera\n Main toh hafton se soya nahi\n\n Bol do na zara\n Dil main jo hai chhipa\n Main kisi se kahunga nahi\n Main kisi se kahunga nahi\n\n");
+printf(" hamari kami tumko mehsoos hongi Bhiga dengi jab Barishein\n Main bhar ke laaya hun Aakho main apni adhuri si khwahishien\n Rooh se chahne vale aashique\n Baatein jismon ki karte nahi\n\n Bol do na zara\n Dil main jo hai chhipa\n Main kisi se kahunga nahi\n Main kisi se kahunga nahi\n");
+sound(b);delay(300);sound(550);delay(300);sound(j);delay(30);sound(555);delay(300);sound(j);delay(30);
+sound(h);delay(300);sound(g);delay(500);nosound();sound(j);delay(300);sound(b);delay(300);sound(550);
+delay(300);sound(j);delay(30);sound(555);delay(300);sound(j);delay(30);sound(h);delay(300);sound(g);
+delay(500);nosound();sound(j);delay(300);sound(b);delay(300);sound(550);delay(300);sound(j);delay(30);
+sound(555);delay(300);sound(j);delay(30);sound(h);delay(300);sound(i);delay(500);nosound();sound(j);
+delay(300);sound(i);delay(300);sound(c);delay(300);sound(k);delay(300);sound(h);delay(300);sound(i);
+delay(500);nosound();sound(j);delay(300);sound(b);delay(300);sound(550);delay(300);sound(j);delay(30);
+sound(555);delay(300);sound(j);delay(30);sound(h);delay(300);sound(g);delay(500);nosound();sound(j);
+delay(300);sound(b);delay(300);sound(550);delay(300);sound(j);delay(30);sound(555);delay(300);
+sound(j);delay(30);sound(h);delay(300);sound(g);delay(500);nosound();sound(j);delay(300);sound(b);
+delay(300);sound(550);delay(300);sound(j);delay(30);sound(555);delay(300);sound(j);delay(30);sound(h);
+delay(300);sound(i);delay(500);nosound();sound(j);delay(300);sound(i);delay(300);sound(c);delay(300);
+sound(k);delay(300);sound(h);delay(300);sound(i);delay(500);nosound();sound(j);delay(300);sound(g);
+delay(300);sound(j);delay(50);sound(g);delay(300);sound(f);delay(300);sound(g);delay(300);sound(b);
+delay(300);sound(g);delay(300);sound(j);delay(50);sound(g);delay(300);sound(j);delay(50);sound(g);
+delay(300);nosound();sound(f);delay(300);sound(i);delay(300);sound(i);delay(300);sound(i);delay(300);
+sound(h);delay(300);sound(h);delay(300);sound(i);delay(300);sound(h);delay(300);sound(g);delay(300);
+sound(b);delay(500);sound(j);delay(50);nosound();sound(g);delay(300);sound(j);delay(50);
+sound(g);delay(300);sound(f);delay(300);sound(g);delay(300);sound(j);delay(50);sound(g);
+delay(300);sound(b);delay(300);sound(g);delay(300);sound(j);delay(50);sound(g);delay(300);sound(j);
+delay(50);sound(g);delay(300);nosound();sound(f);delay(300);sound(i);delay(300);sound(j);delay(50);
+sound(i);delay(300);sound(j);delay(50);sound(i);delay(300);sound(h);delay(300);sound(j);delay(50);
+sound(h);delay(300);sound(i);delay(300);sound(h);delay(300);sound(g);delay(300);sound(b);delay(500);
+nosound();sound(b);delay(300);sound(h);delay(300);sound(i);delay(300);sound(c);delay(300);sound(j);
+delay(50);sound(c);delay(300);sound(i);delay(300);sound(h);delay(300);sound(b);delay(300);sound(h);
+delay(300);sound(g);delay(500);nosound();sound(g);delay(300);sound(b);delay(300);sound(h);delay(300);
+sound(j);delay(50);sound(h);delay(300);sound(b);delay(300);sound(g);delay(300);sound(j);delay(50);
+sound(g);delay(300);sound(f);delay(300);sound(b);delay(500);nosound();sound(b);delay(300);sound(g);
+delay(300);sound(j);delay(50);sound(g);delay(300);sound(a);delay(300);sound(g);delay(300);
+sound(b);delay(300);sound(h);delay(400);sound(j);delay(50);sound(b);delay(300);sound(g);
+delay(300);sound(j);delay(50);sound(g);delay(300);sound(a);delay(300);sound(g);delay(300);sound(b);
+delay(300);sound(h);delay(500);sound(j);delay(50);sound(i);delay(300);sound(h);delay(300);sound(b);
+delay(300);sound(f);delay(300);sound(g);delay(300);sound(b);delay(300);sound(j);delay(50);sound(b);
+delay(300);sound(h);delay(300);sound(g);delay(300);sound(j);delay(200);nosound();sound(i);delay(300);
+sound(h);delay(300);sound(b);delay(300);sound(f);delay(300);sound(g);delay(300);sound(b);delay(300);
+sound(j);delay(50);sound(b);delay(300);sound(h);delay(300);sound(g);delay(600);sound(j);delay(200);
+nosound();sound(f);delay(300);sound(j);delay(50);sound(f);delay(300);sound(g);delay(300);sound(b);
+delay(300);sound(g);delay(300);sound(j);delay(50);sound(g);delay(300);sound(j);delay(50);sound(g);
+delay(300);sound(f);delay(300);sound(j);delay(50);sound(f);delay(300);sound(g);delay(300);sound(b);
+delay(300);sound(g);delay(300);sound(j);delay(50);sound(g);delay(300);sound(j);delay(50);sound(g);
+delay(300);sound(j);delay(50);sound(g);delay(300);sound(f);delay(300);sound(e);delay(300);sound(j);
+delay(50);sound(e);delay(300);sound(j);delay(50);sound(e);delay(300);sound(f);delay(300);sound(d);
+delay(300);sound(a);delay(600);sound(j);delay(200);nosound();sound(f);delay(300);sound(j);delay(50);
+sound(f);delay(300);sound(g);delay(300);sound(b);delay(300);sound(g);delay(300);sound(j);delay(50);
+sound(g);delay(300);sound(j);delay(50);sound(g);delay(300);sound(f);delay(300);sound(j);delay(50);
+sound(f);delay(300);sound(g);delay(300);sound(b);delay(300);sound(g);delay(300);sound(j);delay(50);
+sound(g);delay(300);sound(j);delay(50);sound(g);delay(300);sound(j);delay(50);sound(g);delay(300);
+sound(f);delay(300);sound(e);delay(300);sound(j);delay(50);sound(e);delay(300);sound(j);delay(50);
+sound(e);delay(300);sound(f);delay(300);sound(d);delay(300);sound(a);delay(600);sound(j);delay(200);
+nosound();sound(b);delay(300);sound(h);delay(500);sound(i);delay(300);sound(c);delay(300);sound(j);
+delay(50);sound(c);delay(300);sound(i);delay(300);sound(h);delay(300);sound(b);delay(300);sound(h);
+delay(300);sound(g);delay(500);nosound();sound(g);delay(300);sound(b);delay(300);sound(h);delay(300);
+sound(j);delay(50);sound(h);delay(300);sound(b);delay(300);sound(g);delay(300);sound(j);delay(50);
+sound(g);delay(300);sound(f);delay(300);sound(b);delay(600);sound(j);delay(300);nosound();sound(b);
+delay(300);sound(g);delay(300);sound(j);delay(50);sound(g);delay(300);sound(a);delay(300);sound(g);
+delay(300);sound(b);delay(300);sound(h);delay(400);sound(j);delay(50);sound(b);delay(300);sound(g);
+delay(300);sound(j);delay(50);sound(g);delay(300);sound(a);delay(300);sound(g);delay(300);sound(b);
+delay(300);sound(h);delay(500);sound(j);delay(50);sound(i);delay(300);sound(h);delay(300);sound(b);
+delay(300);sound(f);delay(300);sound(g);delay(300);sound(b);delay(300);sound(j);delay(50);sound(b);
+delay(300);sound(h);delay(300);sound(g);delay(300);sound(j);delay(200);nosound();sound(i);delay(300);
+sound(h);delay(300);sound(b);delay(300);sound(f);delay(300);sound(g);delay(300);sound(b);delay(300);
+sound(j);delay(50);sound(b);delay(300);sound(h);delay(300);sound(g);delay(600);sound(j);delay(200);
+nosound();sound(b);delay(300);sound(550);delay(300);sound(j);delay(30);sound(555);delay(300);sound(j);
+delay(30);sound(h);delay(300);sound(g);delay(500);nosound();sound(j);delay(300);sound(b);delay(300);
+sound(550);delay(300);sound(j);delay(30);sound(555);delay(300);sound(j);delay(30);sound(h);delay(300);
+sound(g);delay(500);nosound();sound(j);delay(300);sound(b);delay(300);sound(550);delay(300);sound(j);
+delay(30);sound(555);delay(300);sound(j);delay(30);sound(h);delay(300);sound(i);delay(500);nosound();
+sound(j);delay(300);sound(i);delay(300);sound(c);delay(300);sound(k);delay(300);sound(h);delay(300);
+sound(i);delay(500);nosound();sound(j);delay(300);nosound();getch();}
