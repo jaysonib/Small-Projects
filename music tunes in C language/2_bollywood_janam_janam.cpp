58e4801f2f86d7b278be09e618c6b13cc6013c92
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 18:22:03 -0400

    Create 2_bollywood_janam_janam.cpp

---
+++ music tunes in C language/2_bollywood_janam_janam.cpp
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+void main()
+{
+clrscr();
+float a=277,b=369,c=415,d=466,e=554,f=739,g=293,h=329,i=391,j=0,k=440,l=493;
+float m=587,n=659;
+//music
+printf(" 'Janam Janam' music tune \n");
+printf(" Lyrics : \n Janam janam janam sath chalna yuhi\n Kasam tumhe kasam aake milna yahi\n Ek jaan hain bhale do badan ho juda\n Meri hoke hamesha hi rehna\n Kabhi na kehna alvidaaaa.......\n\n");
+printf(" Meri subah ho tumhi \n Aur tumhi shaam ho \n Tum dard ho tumhi aaram ho\n Meri duaon se aati hain bas yeh sada\n Meri hoke hamesha hi rehna\n kabhi na kehna alvidaaaa....\n\n\n\n  YOU  CAN  TRY  THIS  PROGRAM  IN TURBO  C++\n\n  LIKE  ,  SHARE  &  SUBSCRIBE");
+sound(l);delay(300);sound(e);delay(300);
+sound(l);delay(300);sound(m);delay(400);nosound();sound(j);delay(10);
+sound(m);delay(300);sound(e);delay(300);
+sound(l);delay(300);sound(i);delay(300);nosound();sound(j);delay(10);
+sound(k);delay(300);nosound();sound(l);delay(300);nosound();
+sound(k);delay(300);sound(e);delay(300);nosound();sound(j);delay(10);
+sound(e);delay(300);sound(l);delay(300);sound(k);delay(300);
+sound(b);delay(300);nosound();sound(j);delay(10);sound(i);delay(300);
+sound(k);delay(300);sound(i);delay(300);
+sound(l);delay(500);nosound();sound(j);delay(10);
+sound(l);delay(300);sound(k);delay(300);sound(i);delay(300);
+sound(h);delay(400);nosound();sound(j);delay(10);sound(h);delay(300);
+sound(b);delay(300);sound(i);delay(300);sound(b);delay(400);
+nosound();sound(j);delay(10);sound(h);delay(300);sound(b);delay(300);
+sound(h);delay(300);sound(g);delay(300);sound(h);delay(300);
+sound(g);delay(300);sound(a);delay(400);nosound();sound(j);delay(10);
+//janam janam janam sath chalna yuhi
+sound(b);delay(190);sound(m);delay(190);sound(j);delay(10);
+sound(m);delay(190);sound(e);delay(190);sound(j);delay(10);
+sound(e);delay(190);sound(l);delay(200);sound(j);delay(10);
+sound(l);delay(200);sound(k);delay(200);sound(b);delay(200);
+sound(i);delay(200);sound(i);delay(200);sound(k);delay(200);
+sound(b);delay(200);sound(i);delay(200);sound(i);delay(200);
+//kasam tum hain kasam aake milna yahi
+sound(h);delay(190);sound(e);delay(190);sound(e);sound(j);delay(5);
+delay(190);sound(l);delay(190);sound(l);sound(j);delay(5);delay(190);
+sound(k);delay(190);sound(k);sound(j);delay(5);delay(200);sound(i);
+delay(200);sound(h);delay(200);sound(b);delay(200);sound(b);delay(200);
+sound(i);delay(200);sound(h);delay(200);sound(b);delay(200);
+//ek hi jaan hain bhale d0 badan ho juda
+sound(g);delay(200);sound(l);delay(200);sound(l);delay(200);sound(k);
+delay(200);sound(k);delay(200);sound(i);delay(200);sound(i);
+delay(200);sound(b);delay(200);sound(g);delay(200);sound(h);
+delay(200);sound(h);delay(200);sound(b);delay(200);sound(g);
+delay(200);sound(h);delay(200);
+//meri hoke hamesha hi rahena kabhi na kahena alwidaaaaaa...
+sound(d);delay(200);sound(l);delay(200);sound(e);delay(200);sound(d);
+delay(200);sound(b);delay(200);sound(c);delay(200);sound(d);delay(200);
+sound(l);delay(200);sound(d);delay(200);sound(l);delay(200);sound(e);
+delay(200);sound(l);delay(200);sound(e);delay(200);sound(m);delay(200);
+sound(e);delay(200);sound(m);delay(200);sound(n);delay(200);sound(m);
+delay(200);sound(n);delay(200);sound(f);delay(600);sound(j);delay(550);
+//meri subah ho tumhi    ,tumhi saam h0
+sound(l);delay(300);sound(e);delay(300);sound(l);delay(300);sound(m);
+delay(300);sound(e);delay(400);sound(e);delay(300);sound(l);delay(300);
+sound(l);delay(300);sound(k);delay(300);sound(b);delay(300);sound(i);
+delay(300);sound(j);delay(20);sound(i);delay(300);sound(k);delay(300);
+sound(b);delay(300);sound(i);delay(300);
+//tum dard ho tumhi aaram h0
+sound(e);delay(300);sound(e);delay(300);sound(l);delay(300);sound(l);
+delay(300);sound(k);delay(300);sound(k);delay(300);sound(i);delay(300);
+sound(h);delay(300);sound(b);delay(300);sound(b);delay(300);sound(i);
+delay(300);sound(h);delay(300);sound(b);delay(300);
+//1 hi jaan hain bhale do badan h0 juda
+sound(g);delay(300);sound(g);delay(300);sound(l);delay(300);sound(l);
+delay(300);sound(k);delay(300);sound(k);delay(300);sound(i);delay(300);
+sound(i);delay(300);sound(b);delay(300);sound(g);delay(300);sound(h);
+delay(300);sound(h);delay(300);sound(b);delay(300);sound(g);delay(300);
+sound(h);delay(300);
+//meri hoke hamesha hi rahena kabhi na kahena alwidaaaa...
+sound(d);delay(200);sound(l);delay(200);sound(e);delay(200);sound(d);
+delay(200);sound(b);delay(200);sound(c);delay(200);sound(d);delay(200);
+sound(l);delay(200);sound(d);delay(200);sound(l);delay(200);sound(e);
+delay(200);sound(l);delay(200);sound(e);delay(200);sound(m);delay(200);
+sound(e);delay(200);sound(m);delay(200);sound(n);delay(200);sound(m);
+delay(200);sound(n);delay(200);sound(f);delay(600);nosound();
+getch();
+}
