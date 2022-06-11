d99c0cafa39f0772663461943868e148f8697e90
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 18:26:16 -0400

    Create 6_vande_mataram.cpp

---
+++ music tunes in C language/6_vande_mataram.cpp
@@ -0,0 +1,87 @@
+#include<stdio.h>
+
+#include<conio.h>
+#include<dos.h>
+void main()
+{
+clrscr();
+int g=391,a=440,b=493,c=523,d=587,e=659,f=698,h=783,i=880;
+int j=987,k=1146,l=1046,m=739,n=932;
+printf("\"Vande Mataram\" - music tune in C language\n Vande mataram Vande mataram \n sujalam suphalam \n malayaja sitalam \n sasya syamlam mataram \n vande mataram vande mataram\n subhra jyotsna pulakita yaminim\n phulla kusumita drumadalasobhinim\n suhasinim sumadhura bhasinim \n sukhdam vardam maataram\n Vande mataram vande mataram\n\n YOU CAN ALSO TRY THIS PROGRAM IN TURBO C++\n FOR MORE VIDEO LIKE , SHARE & SUBSCRIBE \n By :- JBS");
+// Vande mataram Vande mataram
+sound(g);delay(300);nosound();sound(a);delay(500);nosound();sound(c);
+delay(350);nosound();sound(d);delay(350);nosound();sound(c);delay(350);
+nosound();sound(d);delay(500);nosound();sound(0);delay(300);sound(c);
+delay(300);nosound();sound(d);delay(500);nosound();sound(m);delay(350);
+nosound();sound(h);delay(350);nosound();sound(m);delay(350);nosound();
+sound(h);delay(350);nosound();sound(0);delay(350);nosound();
+// sujalam suphalam
+sound(h);delay(300);nosound();sound(i);delay(300);nosound();sound(f);
+delay(300);nosound();sound(e);delay(300);nosound();sound(d);delay(300);
+nosound();sound(0);delay(350);nosound();sound(d);delay(300);nosound();
+sound(e);delay(300);nosound();sound(c);delay(300);nosound();sound(b);
+delay(300);nosound();sound(a);delay(300);nosound();sound(0);delay(350);
+nosound();
+// malayaja sitalam
+sound(a);delay(300);nosound();sound(d);delay(300);nosound();sound(c);
+delay(300);nosound();sound(d);delay(300);nosound();sound(b);delay(300);
+nosound();sound(0);delay(20);nosound();sound(b);delay(300);nosound();
+sound(a);delay(300);nosound();sound(g);delay(300);nosound();sound(0);
+delay(350);nosound();sound(g);delay(350);nosound();sound(a);delay(350);
+nosound();sound(c);delay(350);nosound();sound(d);delay(350);nosound();
+sound(c);delay(350);nosound();sound(d);delay(350);nosound();sound(0);
+delay(350);nosound();
+// sasya syamlam mataram
+sound(d);delay(350);nosound();sound(f);delay(350);nosound();sound(e);
+delay(350);nosound();sound(d);delay(350);nosound();sound(0);delay(350);
+nosound();sound(c);delay(300);nosound();sound(d);delay(500);nosound();
+sound(m);delay(350);nosound();sound(h);delay(350);nosound();sound(m);
+delay(350);nosound();sound(h);delay(350);nosound();sound(0);delay(350);
+nosound();sound(c);delay(300);nosound();sound(d);delay(500);nosound();
+sound(m);delay(350);nosound();sound(h);delay(350);nosound();sound(m);
+delay(350);nosound();sound(h);delay(350);nosound();sound(0);delay(350);
+nosound();
+//vande mataram vande mataram
+sound(f);delay(350);nosound();sound(h);delay(350);nosound();sound(j);
+delay(350);nosound();sound(0);delay(30);nosound();sound(j);delay(350);
+nosound();sound(0);delay(300);nosound();sound(j);delay(250);nosound();
+sound(0);delay(30);nosound();sound(j);delay(250);nosound();sound(l);
+delay(250);nosound();sound(j);delay(250);nosound();sound(l);delay(450);
+nosound();sound(j);delay(250);nosound();sound(l);delay(250);nosound();
+sound(0);delay(250);nosound();
+//subhra jyotsna pulakita yaminim
+sound(j);delay(450);nosound();sound(0);delay(130);nosound();sound(j);
+delay(250);nosound();sound(0);delay(30);nosound();sound(j);delay(250);
+nosound();sound(l);delay(250);nosound();sound(j);delay(250);nosound();
+sound(l);delay(250);nosound();
+// phulla kusumita drumadalasobhinim
+sound(0);delay(250);nosound();sound(l);delay(250);nosound();sound(k);
+delay(250);nosound();sound(l);delay(250);nosound();sound(n);delay(250);
+nosound();sound(0);delay(250);nosound();sound(i);delay(250);nosound();
+sound(n);delay(250);nosound();sound(i);delay(250);nosound();sound(h);
+delay(250);nosound();
+//suhasinim
+sound(d);delay(250);nosound();sound(f);delay(250);nosound();sound(e);
+delay(250);nosound();sound(d);delay(250);nosound();sound(0);delay(250);
+nosound();
+// sumadhura bhasinim
+sound(d);delay(250);nosound();sound(n);delay(250);nosound();sound(i);
+delay(250);nosound();sound(n);delay(350);nosound();sound(i);delay(250);
+nosound();sound(h);delay(250);nosound();sound(i);delay(300);nosound();
+sound(h);delay(250);nosound();sound(0);delay(350);nosound();
+// sukhdam vardam maataram
+sound(f);delay(250);nosound();sound(h);delay(250);nosound();sound(j);
+delay(450);nosound();sound(0);delay(50);nosound();sound(j);delay(250);
+nosound();sound(0);delay(20);nosound();sound(j);delay(250);nosound();
+sound(0);delay(20);nosound();sound(j);delay(450);nosound();sound(0);
+delay(50);nosound();sound(j);delay(350);nosound();sound(l);delay(350);
+nosound();sound(j);delay(350);nosound();sound(l);delay(350);nosound();
+// Vande mataram vande mataram
+sound(c);delay(300);nosound();sound(d);delay(500);nosound();sound(m);
+delay(350);nosound();sound(h);delay(350);nosound();sound(m);delay(350);
+nosound();sound(h);delay(550);nosound();sound(0);delay(250);nosound();
+sound(c);delay(300);nosound();sound(d);delay(500);nosound();sound(m);
+delay(350);nosound();sound(h);delay(350);nosound();sound(m);delay(350);
+nosound();sound(h);delay(550);nosound();
+getch();
+}
