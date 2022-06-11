67c42f3937739a513e81c18f5aa927e499c0b40f
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 18:52:51 -0400

    Create 1_create_and_remove_directory.cpp

---
+++ small_programs/1_create_and_remove_directory.cpp
@@ -0,0 +1,37 @@
+//hii friends . welcome to our you tube channel "Programming Knowledge"
+//here is the program for make directory and remove directory
+//First we create a directory name "programming_knowledge"
+//which is at location C://turboc3//capture ..
+//and then we will see how to remove directory.
+//and then we will see all the files in currnet directory(where the program is saved)
+
+
+
+
+
+
+
+//so let's start
+//we have required these header files
+#include <stdio.h>
+#include <conio.h>
+#include <process.h>
+#include <dir.h>
+
+void main()
+{
+clrscr();
+
+int a;
+//we have to create another name directory beacause we have already make it "program"
+//program is removed and program1 is created now we remove mkdir step it will remove program1 directory
+
+//now we see how to remove it
+//now we check all directory where program is saved
+
+//it is saved in bin directory
+system("dir/p");
+getch();
+
+}
+
