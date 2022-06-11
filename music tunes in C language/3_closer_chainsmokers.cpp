df0079af8a0d2bf47caf5609f0994e34a39b289d
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 18:23:00 -0400

    Create 3_closer_chainsmokers.cpp

---
+++ music tunes in C language/3_closer_chainsmokers.cpp
@@ -0,0 +1,126 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+void main()
+{
+clrscr();
+int a=311,b=349,c=391,d=415,e=466,f=523,g=587,h=622;
+printf(" I was doing just fine before I met you \n I drink too much & that's an issue     But I'm OK\n Tell your friend it was nice to meet them \n But I hope I never see them again \n I know it breaks your heart \n moved to the city in a broke down car and \n four years no calls\n Now you are looking preety in a hotel bar\n and i i i i i can't stop\n no i i i i i  can't stop \n so baby pull me closer\n in the backseat of your rover \n that i know you can't afford\n Bite the tatto on your shoulder\n pull the sheet right off the corner\n of the mattress that your stole \n from your roommate back in boulder\n we ain't ever getting older\n music \n\n   YOU CAN TRY THIS PROGRAM IN TURBO C++\n\n   LIKE  SHARE  &  SUBSCRIBE ");
+//I was doing just fine before I met you I drink too much & that's an issue     But I'm OK
+sound(h);delay(300);nosound();sound(0);delay(300);nosound();
+sound(d);delay(200);nosound();sound(0);delay(10);nosound();sound(d);
+delay(300);nosound();sound(h);delay(300);nosound();sound(d);delay(300);
+nosound();sound(f);delay(200);nosound();sound(0);delay(10);nosound();
+sound(f);delay(300);nosound();sound(e);delay(200);
+nosound();sound(0);delay(10);nosound();sound(e);delay(200);
+nosound();sound(0);delay(10);nosound();sound(e);delay(300);nosound();
+sound(d);delay(300);nosound();sound(h);delay(200);nosound();sound(0);
+delay(10);nosound();sound(h);delay(200);nosound();sound(0);delay(10);
+nosound();sound(h);delay(300);nosound();sound(f);delay(200);nosound();
+sound(0);delay(10);nosound();sound(f);delay(300);nosound();sound(e);
+delay(200);nosound();sound(0);delay(10);nosound();sound(e);delay(200);
+nosound();sound(0);delay(10);nosound();sound(e);delay(300);nosound();sound(d);
+delay(300);nosound();sound(f);delay(300);nosound();sound(e);delay(300);
+nosound();sound(0);delay(500);nosound();
+//Tell your friend it was nice to meet them  But I hope I never see them again
+sound(h);delay(300);nosound();sound(0);delay(10);nosound();sound(h);
+delay(300);nosound();sound(0);delay(10);nosound();sound(h);delay(300);
+nosound();sound(f);delay(300);nosound();sound(0);delay(10);nosound();
+sound(f);delay(300);nosound();sound(e);delay(300);nosound();sound(0);
+delay(10);nosound();sound(e);delay(300);nosound();sound(0);delay(10);
+nosound();sound(e);delay(300);nosound();sound(d);delay(300);nosound();
+sound(0);delay(10);nosound();sound(d);delay(300);nosound();sound(f);
+delay(300);nosound();sound(0);delay(10);nosound();sound(f);delay(300);
+nosound();sound(e);delay(300);nosound();sound(0);delay(10);nosound();
+sound(e);delay(300);nosound();sound(d);delay(300);nosound();sound(0);
+delay(10);nosound();sound(d);delay(300);nosound();sound(0);delay(10);
+nosound();sound(d);delay(300);nosound();sound(b);delay(300);nosound();
+sound(f);delay(300);nosound();sound(e);delay(300);nosound();sound(0);
+delay(500);nosound();
+//I know it breaks your heart moved to the city in a broke down car and
+//four years no calls Now you are looking preety in a hotel bar
+sound(d);delay(300);nosound();sound(0);delay(50);nosound();sound(d);
+delay(300);nosound();sound(0);delay(10);nosound();sound(d);delay(300);
+nosound();sound(0);delay(10);nosound();sound(d);delay(300);nosound();
+sound(0);delay(10);nosound();sound(d);delay(300);nosound();sound(e);
+delay(300);nosound();sound(0);delay(10);nosound();sound(e);delay(300);
+nosound();sound(d);delay(300);nosound();sound(0);delay(10);nosound();
+sound(d);delay(300);nosound();sound(0);delay(10);nosound();sound(d);
+delay(300);nosound();sound(0);delay(10);nosound();sound(d);delay(300);
+nosound();sound(0);delay(10);nosound();sound(d);delay(300);nosound();sound(0);
+delay(10);nosound();sound(d);delay(300);nosound();sound(0);delay(10);
+nosound();sound(d);delay(300);nosound();sound(e);delay(300);nosound();
+sound(0);delay(10);nosound();sound(e);delay(300);nosound();sound(d);
+delay(300);nosound();sound(0);delay(10);nosound();sound(d);delay(300);
+nosound();sound(0);delay(10);nosound();sound(d);delay(300);nosound();sound(0);
+delay(10);nosound();sound(d);delay(300);nosound();sound(e);delay(300);
+nosound();sound(0);delay(10);nosound();sound(e);delay(300);nosound();sound(d);
+delay(300);nosound();sound(0);delay(10);nosound();sound(d);delay(300);
+nosound();sound(0);delay(10);nosound();sound(d);delay(300);nosound();sound(0);
+delay(10);nosound();sound(d);delay(300);nosound();sound(0);delay(10);
+nosound();sound(d);delay(300);nosound();sound(0);delay(10);nosound();
+sound(d);delay(300);nosound();sound(0);delay(10);nosound();sound(d);
+delay(300);nosound();sound(0);delay(10);nosound();sound(d);delay(300);
+nosound();sound(e);delay(300);nosound();sound(0);delay(10);nosound();sound(e);
+delay(300);nosound();
+//and i-i-i-i-i can't stop
+sound(d);delay(300);nosound();sound(h);delay(200);nosound();sound(f);
+delay(350);nosound();sound(e);delay(200);nosound();sound(d);delay(350);
+nosound();sound(a);delay(350);nosound();sound(f);delay(300);nosound();
+sound(e);delay(300);nosound();sound(0);delay(500);nosound();
+//No i-i-i-i-i can't stop
+sound(d);delay(300);nosound();sound(h);delay(200);nosound();sound(f);
+delay(350);nosound();sound(e);delay(200);nosound();sound(d);delay(350);
+nosound();sound(a);delay(350);nosound();sound(f);delay(300);nosound();
+sound(e);delay(300);nosound();sound(0);delay(500);nosound();
+//so baby pull me closer in the backseat of your rover
+//that i know you can't afford Bite the tatto on your shoulder
+//pull the sheet right off the corner of the mattress taht your stole
+//from your roommate back in boulder we ain't ever getting older
+sound(d);delay(300);sound(e);delay(300);sound(f);delay(300);sound(d);
+delay(300);sound(e);delay(300);nosound();sound(0);delay(20);nosound();
+sound(e);delay(300);sound(f);delay(300);sound(d);delay(300);sound(e);
+delay(300);nosound();sound(0);delay(20);nosound();sound(e);delay(300);
+sound(f);delay(300);sound(d);delay(300);sound(e);delay(300);
+nosound();sound(0);delay(20);nosound();sound(e);delay(300);sound(d);
+delay(300);nosound();sound(0);delay(20);nosound();sound(d);delay(300);
+sound(e);delay(300);nosound();sound(0);delay(20);nosound();sound(e);
+delay(300);sound(d);delay(300);nosound();sound(0);delay(20);nosound();
+sound(d);delay(300);sound(e);delay(300);nosound();sound(0);delay(20);
+nosound();sound(e);delay(300);sound(d);delay(300);nosound();sound(0);
+delay(20);nosound();sound(g);delay(300);sound(e);delay(300);nosound();
+sound(0);delay(20);nosound();sound(e);delay(300);sound(f);delay(300);
+sound(d);delay(300);sound(e);delay(300);nosound();sound(0);delay(20);
+nosound();sound(e);delay(300);sound(d);delay(300);nosound();sound(0);
+delay(20);nosound();sound(d);delay(300);sound(e);delay(300);nosound();
+sound(0);delay(20);nosound();sound(e);delay(300);sound(f);delay(300);sound(d);
+delay(300);sound(e);delay(300);nosound();sound(0);delay(20);nosound();
+sound(e);delay(300);sound(f);delay(300);sound(d);delay(300);sound(e);
+delay(300);nosound();sound(0);delay(20);nosound();sound(e);delay(300);
+sound(f);delay(300);sound(d);delay(300);sound(e);delay(300);nosound();
+sound(0);delay(20);nosound();sound(e);delay(300);sound(d);delay(300);
+nosound();sound(0);delay(20);nosound();sound(d);delay(300);sound(e);
+delay(300);nosound();sound(0);delay(20);nosound();sound(e);delay(300);
+sound(d);delay(300);sound(d);delay(300);sound(e);delay(300);nosound();
+sound(0);delay(20);nosound();sound(e);delay(300);sound(d);delay(300);
+nosound();sound(0);delay(20);nosound();sound(d);delay(300);sound(f);
+delay(300);nosound();sound(0);delay(20);nosound();sound(f);delay(300);
+sound(d);delay(300);nosound();sound(0);delay(20);nosound();sound(d);
+delay(300);sound(e);delay(300);nosound();sound(0);delay(20);nosound();
+sound(e);delay(300);nosound();sound(0);delay(20);nosound();sound(e);
+delay(300);sound(d);delay(300);nosound();sound(0);delay(20);nosound();
+sound(d);delay(300);sound(e);delay(300);nosound();sound(0);delay(20);
+nosound();sound(e);delay(300);sound(d);delay(300);nosound();sound(0);
+delay(20);nosound();sound(d);delay(300);nosound();sound(d);delay(300);
+sound(e);delay(300);sound(d);delay(300);nosound();sound(0);delay(20);
+nosound();sound(d);delay(300);sound(f);delay(300);nosound();sound(0);
+delay(20);nosound();sound(f);delay(300);nosound();sound(0);delay(20);
+nosound();sound(f);delay(300);sound(d);delay(300);sound(d);delay(300);
+sound(e);delay(300);nosound();sound(0);delay(20);nosound();sound(e);
+delay(300);sound(d);delay(300);nosound();sound(0);delay(20);nosound();
+sound(d);delay(300);sound(e);delay(300);nosound();sound(0);delay(20);
+nosound();sound(e);delay(300);sound(d);delay(300);nosound();sound(0);
+delay(20);nosound();sound(d);delay(300);sound(e);delay(300);nosound();
+sound(0);delay(20);nosound();sound(e);delay(300);nosound();
+getch();
+}
