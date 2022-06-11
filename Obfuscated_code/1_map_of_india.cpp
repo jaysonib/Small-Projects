3a90902cb9d8f4c99682d14a141dfe5f7769725a
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 19:51:45 -0400

    Create 1_map_of_india.cpp

---
+++ Obfuscated_code/1_map_of_india.cpp
@@ -0,0 +1,56 @@
+// Hello Friends!
+// Welcome to my YouTube channel Programming Knowledge.
+// Like this video, share this code with your friends and subscribe my channel.
+#include<stdio.h>
+
+              //INDIA
+              int main()
+               {int c,m,i,j;;;
+                 int key=1<<2;;
+               for(i=0,m=1<<5|
+                1;c="ApmN01"
+                "F[1]*==A+N"
+                  "++J=-w+J"
+                  "=AAOP@"
+                  "OSYqmr=A"
+                 "iQ=ONo1p1@"
+               "=A3OoNOngqxv=A"
+              "&Po0L=Isrg=A1R"                                  "NIL"
+            "++q()=AxR=AH=Au()"                             "=AuR@+Ja"
+           "yson=A1Q()N=App+=A("/*                 P         rogrammi*/
+      "O)+P(-p)+=A!N=APp=h_eE10D6f="              /*     ngknowledge*/
+     "A1L=AS-=e{}J@yAry=A[K]=WinQ+sAcHIn="        /*      100*IN*/
+      "JayB=S=A=FInd1@N=AB=AE+=L++D!!=A!E!!k=AH++B01F=AH=AG!!="
+       "A#Fun4l1Fe+=A!G=At=AAheLl0G0d=A#Hi=r+=Bo0L" "{(G)}=A!B!"
+        "PFr0AgRqaMHm1FnGHkN=A0Aw1zeDFgEE+lIke=A;D("  "+v,-H,*C)="
+  "J@y=" "A+B=-z=bH@rAt=Jbs=A1DotF=mArsmisW0n=A+E()"        "E+=A"
+ "26kT@Y10=ArLSWiFtZ()=A+Kk=ii=a0t=A+L9%d^1e01=A!L!!"      "d0n"
+    "e%A=A*L()bBeg1n=AlL(+Z,+i++=A=Lj@Yt@j!!=A+MaxX"        "{"
+  "jj,}=A!N@nThem0f=A-N=ISr0n()=A2O21P[*pq]=ApP1ePq"//      !
+    "Po01" "=ApPr_O()pri=AtQ11N=@pqr=A*ReALMeq++="
+     "AIR" "nuLl=q++=A+Re@L=Iq()=A+T=tHi_sis=AI"
+            "T+=Fl_u1d=A(U)=Ddiv++"/**https://
+           youtube.com/c/programmingknowledge0
+            01/videos****JAY****/[!(c-(1<<
+           6)+3)?(m=(1<<3)+(1<<3)+(1<<4),(
+            printf("\n"))):(m^=1),i+=3];//
+            ){for(j=(1<<3)-(key*2);(c-
+            (((1<<10)/(key*2))/key+29))
+             &&(j++-c-(c<(1<<6)?1+key
+              /**made by :-*******
+              ****programming****
+               ***knowledge*/:(c>
+                ((3<<5)-key-(1<<
+                1)))?(4<<5)-(3
+                 <<6)-key-(1<<1)
+                  :(4<<5)-((3
+                  <<6))));//a
+                  printf("%c",
+                    m));};;;
+                   return
+                     0;;}
+					 
+					 
+					 
+					 
+// obfuscation,c language,obfuscated code in c language,code obfuscation techniques,obfuscation in c,programming language,obfuscated code,code obfuscation,code obfuscation introduction,basics of code obfuscation,how to print map of india in c language,c obfuscation contest,c obfuscation techniques,obfuscated c code,obfuscated c code contest,language c,print map of india in c language,map of india in C,map of india,IOCCC,IOCCC contest,IOCCC contest 2021,#IOCCC					 
