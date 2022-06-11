1cd517e2e07c547f2f4b69fe977d777946960105
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 18:54:21 -0400

    Create 20_theher_ja_october.cpp

---
+++ music tunes in C language/20_theher_ja_october.cpp
@@ -0,0 +1,149 @@
+//Hii friends ..
+//Welcome to our you tube channel "Programming Knowledge"
+//This is the video for Varun Dhawan new movie "October" - song "Thehar ja"
+//This song is sang by Arman Malik
+//I have make it's music tune in C language
+//I hope you guys enjoy it
+//First we will see the output and then we will check program
+//Let's run it
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+#include<graphics.h>
+void main()
+{
+clrscr();
+int gd=DETECT,gm;
+initgraph(&gd,&gm,"C:\\turboc3\\bgi");
+
+settextstyle(3,HORIZ_DIR,4);
+setcolor(YELLOW);
+outtextxy(30,200,"MADE BY :- JAY B. SONI");
+
+delay(500);
+setcolor(LIGHTBLUE);
+outtextxy(30,300,"MADE BY :- JAY B. SONI");
+
+delay(500);
+setcolor(LIGHTGREEN);
+outtextxy(30,100,"MADE BY :- JAY B. SONI");
+
+printf("\n                        \n  Hi friends!! Welcome to our you tube channel \" Programming Knowledge\"\n  Here is the video of Varun Dhawan song \"Thehar Ja\" from movie \"October\" \n  Singer - Arman Malik\n  Here is the lyrics :- ");
+
+int a=277,b=311,c=329,d=369,e=415,f=440,g=493;
+
+printf("\n  Saal badla , Haal badla");
+sound(f);delay(250);nosound();sound(0);delay(20);nosound();sound(g);
+delay(250);nosound();sound(0);delay(20);nosound();sound(f);delay(250);
+nosound();sound(0);delay(20);nosound();sound(f);delay(250);nosound();
+sound(0);delay(20);nosound();sound(e);delay(250);nosound();sound(0);
+delay(20);nosound();sound(f);delay(350);nosound();sound(0);delay(50);
+nosound();sound(d);delay(250);nosound();sound(0);delay(20);nosound();
+sound(e);delay(250);nosound();sound(0);delay(20);nosound();sound(e);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(e);delay(250);nosound();
+sound(0);delay(50);nosound();
+printf("\n  Tere aane se");
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(c);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(e);delay(250);nosound();
+sound(0);delay(20);nosound();sound(e);delay(250);nosound();sound(0);
+delay(20);nosound();sound(d);delay(250);nosound();sound(0);delay(250);
+nosound();
+printf("\n  Zindagi ka khayal badla");
+sound(g);delay(250);nosound();sound(0);delay(20);nosound();sound(f);
+delay(250);nosound();sound(0);delay(20);nosound();sound(f);delay(250);
+nosound();sound(0);delay(20);nosound();sound(f);delay(250);nosound();
+sound(0);delay(60);nosound();sound(c);delay(250);nosound();sound(0);
+delay(20);nosound();sound(d);delay(250);nosound();sound(0);delay(20);
+nosound();sound(e);delay(250);nosound();sound(0);delay(20);nosound();
+sound(e);delay(250);nosound();sound(0);delay(20);nosound();sound(d);
+delay(250);nosound();sound(0);delay(20);nosound();sound(e);delay(250);
+nosound();sound(0);delay(50);nosound();
+printf("\n  Tere aane se");
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(c);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(e);delay(250);nosound();
+sound(0);delay(20);nosound();sound(e);delay(250);nosound();sound(0);
+delay(20);nosound();sound(d);delay(250);nosound();sound(0);delay(250);
+nosound();
+printf("\n  Zara zara sa...");
+sound(g);delay(250);nosound();sound(0);delay(20);nosound();sound(a*2);
+delay(250);nosound();sound(0);delay(20);nosound();sound(b*2);delay(250);
+nosound();sound(0);delay(20);nosound();sound(b*2);delay(250);nosound();
+sound(0);delay(20);nosound();sound(c*2);delay(450);nosound();sound(0);
+delay(20);nosound();sound(g);delay(350);nosound();sound(0);delay(100);
+nosound();
+printf("\n  Dooba dooba rehta hoon");
+sound(g);delay(250);nosound();sound(0);delay(20);nosound();sound(a*2);
+delay(250);nosound();sound(0);delay(20);nosound();sound(b*2);delay(250);
+nosound();sound(0);delay(20);nosound();sound(c*2);delay(250);nosound();
+sound(0);delay(20);nosound();sound(b*2);delay(250);nosound();sound(0);
+delay(20);nosound();sound(a*2);delay(350);nosound();sound(0);delay(40);
+nosound();sound(a*2);delay(250);nosound();sound(0);delay(20);nosound();
+sound(b*2);delay(250);nosound();sound(0);delay(20);nosound();sound(g);
+delay(250);nosound();sound(0);delay(250);nosound();
+printf("\n  Zara zara sa...");
+sound(g);delay(250);nosound();sound(0);delay(20);nosound();sound(a*2);
+delay(250);nosound();sound(0);delay(20);nosound();sound(b*2);delay(250);
+nosound();sound(0);delay(20);nosound();sound(b*2);delay(250);nosound();
+sound(0);delay(20);nosound();sound(c*2);delay(450);nosound();sound(0);
+delay(20);nosound();sound(g);delay(350);nosound();sound(0);delay(100);
+nosound();
+printf("\n  Dooba dooba tujh mein");
+sound(g);delay(250);nosound();sound(0);delay(20);nosound();sound(a*2);
+delay(250);nosound();sound(0);delay(20);nosound();sound(b*2);delay(250);
+nosound();sound(0);delay(20);nosound();sound(c*2);delay(250);
+nosound();sound(0);delay(20);nosound();sound(b*2);delay(250);nosound();
+sound(0);delay(20);nosound();sound(a*2);delay(350);nosound();sound(0);
+delay(40);nosound();sound(a*2);delay(250);nosound();sound(0);delay(20);
+nosound();sound(b*2);delay(250);nosound();sound(0);delay(20);nosound();
+sound(g);delay(250);nosound();sound(0);delay(250);nosound();
+printf("\n  Thehar jaa, hmm...");
+sound(g);delay(270);nosound();sound(0);delay(20);nosound();sound(g);
+delay(270);nosound();sound(0);delay(20);nosound();sound(f);delay(270);
+nosound();sound(0);delay(20);nosound();sound(e);delay(270);nosound();
+sound(0);delay(60);nosound();sound(0);delay(60);nosound();
+printf("\n  Tu kisi bahane se");
+sound(d);delay(270);nosound();sound(0);delay(20);nosound();sound(c);
+delay(270);nosound();sound(0);delay(20);nosound();sound(d);delay(270);
+nosound();sound(0);delay(20);nosound();sound(c);delay(270);nosound();
+sound(0);delay(20);nosound();sound(d);delay(270);nosound();sound(0);
+delay(20);nosound();sound(e);delay(270);nosound();sound(0);delay(20);
+nosound();sound(d);delay(370);nosound();sound(0);delay(250);nosound();
+printf("\n  Thehar jaa, hmm...");
+sound(g);delay(270);nosound();sound(0);delay(20);nosound();sound(g);
+delay(270);nosound();sound(0);delay(20);nosound();sound(f);delay(270);
+nosound();sound(0);delay(20);nosound();sound(e);delay(270);nosound();
+sound(0);delay(60);nosound();sound(d);delay(270);nosound();sound(e);
+delay(270);nosound();sound(0);delay(60);nosound();
+printf("\n  Tu kisi bahane se");
+sound(d);delay(270);nosound();sound(0);delay(20);nosound();sound(c);
+delay(270);nosound();sound(0);delay(20);nosound();sound(d);delay(270);
+nosound();sound(0);delay(20);nosound();sound(c);delay(270);nosound();
+sound(0);delay(20);nosound();sound(d);delay(270);nosound();sound(0);
+delay(20);nosound();sound(e);delay(270);nosound();sound(0);delay(20);
+nosound();sound(d);delay(370);nosound();sound(0);delay(250);nosound();
+printf("\n  Wo o wo....");
+printf("\n  Wo o wo....");
+sound(g);delay(270);nosound();sound(0);delay(20);nosound();sound(a*2);
+delay(270);nosound();sound(0);delay(20);nosound();sound(b*2);delay(270);
+nosound();sound(0);delay(20);nosound();sound(c*2);delay(270);nosound();
+sound(0);delay(20);nosound();sound(b*2);delay(200);nosound();sound(0);
+delay(20);nosound();sound(c*2);delay(270);nosound();sound(0);delay(20);
+nosound();sound(g);delay(270);nosound();sound(0);delay(20);nosound();
+sound(a*2);delay(270);nosound();sound(0);delay(20);
+nosound();sound(b*2);delay(270);nosound();sound(0);delay(20);nosound();
+sound(c*2);delay(270);nosound();sound(0);delay(20);nosound();sound(b*2);
+delay(250);nosound();sound(0);delay(20);nosound();sound(c*2);delay(250);
+nosound();sound(0);delay(20);nosound();sound(a*2);delay(250);nosound();
+sound(0);delay(20);nosound();sound(g);delay(250);nosound();sound(0);
+delay(20);nosound();sound(g);delay(250);nosound();
+printf("\n  \n                        \n  Like , share and subscribe for more stuff \n  Visit our channel to find fabulous videos \n  Jay Hind.\n                        ");
+getch();
+}
+
+//Thanks for watching this video
+//Visit our channel , you can find amazing stuff of programming
+//Like , share and subscribe
+//JAY HIND
