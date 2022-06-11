4511c27285d3aa628a301e004ec642c8275aed78
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 18:27:39 -0400

    Create 7_tum_hi_ho_aashiqui_2.cpp

---
+++ music tunes in C language/7_tum_hi_ho_aashiqui_2.cpp
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+void main()
+{
+clrscr();
+int a=311,b=349,c=391,d=415,e=466,f=523,g=554,h=622,i=698;
+printf("Tum Hi Ho :- AASHIQUI 2 SONG - music tune in C language\n\n  music\n  Hum tere binn ab rehh nahi sakte tere bina kya wajoodh mera\n  music \n  Hum tere binn ab rehh nahi sakte tere bina kya wajoodh mera\n  Tujhse juda agar ho jaayenge to khud se bhi ho jayyenge judaa \n  Kyunki tum hi ho \n  Ab tum hi ho\n  Zindagi ab tum hi ho \n  Chain bhi mera dard bhi \n  Meri aashiqui ab tum hi ho \n  music\n\n\n\n                             \n YOU CAN TRY THIS PROGRAM IN TURBO C++\n BY :-) JBS \n                            ");
+//music
+for(int loop=0;loop<4;loop++)
+{sound(b);delay(300);nosound();sound(d);delay(300);nosound();sound(f);
+delay(400);nosound();sound(g);delay(300);nosound();sound(d);delay(300);
+nosound();sound(e);delay(300);nosound();sound(c);delay(400);nosound();
+}
+// Hum tere binn ab rehh nahi sakte tere bina kya wajoodh mera
+sound(0);delay(300);sound(f);delay(300);nosound();sound(i);delay(300);
+nosound();sound(h);delay(300);nosound();sound(0);delay(20);nosound();
+sound(h);delay(300);nosound();sound(g);delay(300);nosound();sound(0);
+delay(20);nosound();sound(g);delay(300);nosound();sound(f);delay(300);
+nosound();sound(e);delay(300);nosound();sound(d);delay(300);nosound();
+sound(e);delay(300);nosound();sound(g);delay(300);nosound();sound(f);
+delay(300);nosound();sound(0);delay(20);nosound();sound(f);delay(300);
+nosound();sound(e);delay(300);nosound();sound(0);delay(20);nosound();
+sound(e);delay(300);nosound();sound(d);delay(300);nosound();sound(e);
+delay(300);nosound();sound(0);delay(20);nosound();sound(e);delay(300);
+nosound();sound(d);delay(300);nosound();sound(c);delay(300);nosound();
+sound(b);delay(300);nosound();sound(a);delay(300);nosound();sound(b);
+delay(700);nosound();
+//  music   Hum tere binn ab rehh nahi sakte tere bina kya wajoodh mera
+sound(b);delay(300);nosound();sound(d);delay(300);nosound();sound(f);
+delay(400);nosound();sound(g);delay(300);nosound();sound(d);delay(300);
+nosound();sound(e);delay(300);nosound();sound(c);delay(400);nosound();
+sound(b);delay(300);nosound();sound(d);delay(300);nosound();sound(f);
+delay(400);nosound();sound(g);delay(300);nosound();sound(d);delay(300);
+nosound();sound(e);delay(300);nosound();sound(c);delay(400);nosound();
+sound(0);delay(300);sound(f);delay(300);nosound();sound(i);delay(300);
+nosound();sound(h);delay(300);nosound();sound(0);delay(20);nosound();
+sound(h);delay(300);nosound();sound(g);delay(300);nosound();sound(0);
+delay(20);nosound();sound(g);delay(300);nosound();sound(f);delay(300);
+nosound();sound(e);delay(300);nosound();sound(d);delay(300);nosound();
+sound(e);delay(300);nosound();sound(g);delay(300);nosound();sound(f);
+delay(300);nosound();sound(0);delay(20);nosound();sound(f);delay(300);
+nosound();sound(e);delay(300);nosound();sound(0);delay(20);nosound();
+sound(e);delay(300);nosound();sound(d);delay(300);nosound();sound(e);
+delay(300);nosound();sound(0);delay(20);nosound();sound(e);delay(300);
+nosound();sound(d);delay(300);nosound();sound(c);delay(300);nosound();
+sound(b);delay(300);nosound();sound(a);delay(300);nosound();sound(b);
+delay(500);nosound();
+// Tujhse juda agar ho jaayenge to khud se bhi ho jayyenge judaa
+sound(f);delay(300);nosound();sound(0);delay(30);nosound();sound(f);
+delay(300);nosound();sound(i);delay(300);nosound();sound(h);delay(300);
+nosound();sound(g);delay(300);nosound();sound(0);delay(30);nosound();
+sound(g);delay(300);nosound();sound(f);delay(300);nosound();sound(d);
+delay(300);nosound();sound(e);delay(300);nosound();sound(g);delay(300);
+nosound();sound(f);delay(300);nosound();sound(0);delay(30);nosound();
+sound(f);delay(300);nosound();sound(e);delay(300);nosound();sound(0);
+delay(30);nosound();sound(e);delay(300);nosound();sound(d);delay(300);
+nosound();sound(e);delay(300);nosound();sound(0);delay(30);nosound();
+sound(e);delay(300);nosound();sound(d);delay(300);nosound();sound(c);
+delay(300);nosound();sound(b);delay(300);nosound();sound(a);delay(300);
+nosound();sound(b);delay(300);nosound();
+// Kyunki tum hi ho
+sound(d);delay(200);nosound();sound(e);delay(200);nosound();sound(f);
+delay(300);nosound();sound(d);delay(300);nosound();sound(e);delay(300);
+nosound();sound(0);delay(300);
+// Ab tum hi ho
+sound(d);delay(200);nosound();sound(e);delay(200);nosound();sound(f);
+delay(300);nosound();sound(d);delay(300);nosound();sound(e);delay(300);
+nosound();sound(0);delay(300);
+// Zindagi ab tum hi ho   Chain bhi mera dard bhi
+// Meri aashiqui ab tum hi ho
+sound(c);delay(400);nosound();sound(e);delay(400);nosound();sound(d);
+delay(400);nosound();sound(0);delay(100);nosound();sound(c);delay(200);
+nosound();sound(b);delay(200);nosound();sound(c);delay(200);nosound();
+sound(a);delay(200);nosound();sound(b);delay(200);nosound();sound(c);
+delay(300);nosound();sound(d);delay(300);nosound();sound(e);delay(300);
+nosound();sound(f);delay(300);nosound();sound(d);delay(300);nosound();
+sound(e);delay(300);nosound();sound(d);delay(300);nosound();sound(e);
+delay(300);nosound();sound(f);delay(300);nosound();sound(d);delay(300);
+nosound();sound(e);delay(300);nosound();
+//  music
+sound(c);delay(400);nosound();sound(e);delay(400);nosound();sound(d);
+delay(400);nosound();sound(c);delay(200);nosound();sound(b);delay(200);
+nosound();sound(c);delay(200);nosound();sound(a);delay(200);nosound();
+sound(b);delay(200);nosound();sound(b);delay(300);nosound();sound(d);
+delay(300);nosound();sound(f);delay(400);nosound();sound(g);delay(300);
+nosound();sound(d);delay(300);nosound();sound(e);delay(300);nosound();
+sound(c);delay(400);nosound();sound(b);delay(300);nosound();sound(d);
+delay(300);nosound();sound(f);delay(400);nosound();sound(g);delay(300);
+nosound();sound(d);delay(300);nosound();sound(e);delay(300);nosound();
+sound(c);delay(400);nosound();getch();
+}
