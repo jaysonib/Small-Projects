3919d56a9c2d09cbf464cf513d61bd7c056338a3
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 18:49:24 -0400

    Create 19_havana_camila_cabello.cpp

---
+++ music tunes in C language/19_havana_camila_cabello.cpp
@@ -0,0 +1,286 @@
+//Here is the code for famous song of 2017  Havana - Camila Cabello ft. Young Thug - song music in C language
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+#include<graphics.h>
+void main()
+{
+clrscr();
+int gd=DETECT,gm;
+initgraph(&gd,&gm,"C:\\turboc3\\bgi");
+
+settextstyle(3,HORIZ_DIR,4);
+setcolor(YELLOW);
+outtextxy(30,200,"MADE BY :- JAY B. SONI");
+
+delay(500);
+setcolor(LIGHTBLUE);
+outtextxy(30,300,"MADE BY :- JAY B. SONI");
+
+delay(500);
+setcolor(LIGHTGREEN);
+outtextxy(30,100,"MADE BY :- JAY B. SONI");
+
+
+
+int a=440,b=466,c=523,d=587,f=349,e=622,g=391;
+printf("\n                        \n  Welcome to our you tube channel \" Programming Knowledge\"\n  Here is the video of famous song of 2017 \" Havana\" - Camila Cabello ft. Young Thug - song music in C language\n  Here is the lyrics :- ");
+printf("\n  Havana, ooh na-na");
+sound(b);delay(250);nosound();sound(0);delay(20);nosound();sound(d);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(b);delay(250);nosound();
+sound(0);delay(20);nosound();sound(b);delay(250);nosound();sound(0);delay(20);
+nosound();sound(g);delay(250);nosound();sound(0);delay(20);nosound();
+printf("\n  Half of my heart is in Havana, ooh-na-na");
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(c);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(e);delay(250);nosound();
+sound(0);delay(20);nosound();sound(d);delay(250);nosound();sound(0);
+delay(20);nosound();sound(c);delay(250);nosound();sound(0);delay(20);
+nosound();sound(b);delay(250);nosound();sound(0);delay(20);nosound();
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(d);
+delay(250);nosound();sound(0);delay(20);nosound();sound(b);delay(250);
+nosound();sound(0);delay(20);nosound();sound(b);delay(250);nosound();
+sound(0);delay(20);nosound();sound(g);delay(250);nosound();sound(0);
+delay(20);nosound();
+
+printf("\n  He took me back to East Atlana, na-na-na");
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(c);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(e);delay(250);nosound();
+sound(0);delay(20);nosound();sound(d);delay(250);nosound();sound(0);
+delay(20);nosound();sound(c);delay(250);nosound();sound(0);delay(20);
+nosound();sound(b);delay(250);nosound();sound(0);delay(20);nosound();
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(d);
+delay(250);nosound();sound(0);delay(20);nosound();sound(b);delay(250);
+nosound();sound(0);delay(20);nosound();sound(b);delay(250);nosound();
+sound(0);delay(20);nosound();sound(g);delay(250);nosound();sound(0);
+delay(20);nosound();
+
+printf("\n  All but my heart is in Havana");
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(c);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(e);delay(250);nosound();
+sound(0);delay(20);nosound();sound(d);delay(250);nosound();sound(0);
+delay(20);nosound();sound(c);delay(250);nosound();sound(0);delay(20);
+nosound();sound(b);delay(250);nosound();sound(0);delay(20);nosound();
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(b);
+delay(250);nosound();sound(0);delay(20);nosound();
+
+printf("\n  There's something about his manners");
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(c);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(c);delay(250);nosound();
+sound(0);delay(20);nosound();sound(b);delay(250);nosound();sound(0);
+delay(20);nosound();sound(d);delay(250);nosound();sound(0);delay(20);
+nosound();sound(a);delay(250);nosound();sound(0);delay(20);nosound();
+
+printf("\n  Havana, ooh-na");
+sound(b);delay(250);nosound();sound(0);delay(20);nosound();sound(a);
+delay(250);nosound();sound(0);delay(20);nosound();sound(b);delay(250);
+nosound();sound(0);delay(20);nosound();sound(a);delay(250);nosound();
+sound(0);delay(20);nosound();sound(b);delay(250);nosound();sound(0);
+delay(20);nosound();
+
+printf("\n  He didn't walk up with that \"How you doing?\"");
+sound(d*2);delay(200);nosound();sound(0);delay(20);nosound();sound(g*2);
+delay(180);nosound();sound(0);delay(20);nosound();sound(g*2);delay(180);
+nosound();sound(0);delay(20);nosound();sound(g*2);delay(180);nosound();
+sound(0);delay(20);nosound();sound(d*2);delay(180);nosound();sound(0);
+delay(20);nosound();sound(d*2);delay(180);nosound();sound(0);delay(20);
+nosound();sound(d*2);delay(180);nosound();sound(0);delay(20);nosound();
+sound(e*2);delay(180);nosound();sound(0);delay(20);nosound();sound(d*2);
+delay(200);nosound();sound(0);delay(20);nosound();sound(c*2);delay(200);
+nosound();sound(0);delay(20);nosound();sound(d*2);delay(200);nosound();
+sound(0);delay(50);nosound();
+
+printf("\n  (When he came into the room)");
+sound(d*2);delay(200);nosound();sound(0);delay(20);nosound();sound(c*2);
+delay(200);nosound();sound(0);delay(20);nosound();sound(d*2);delay(200);
+nosound();sound(0);delay(20);nosound();sound(c*2);delay(200);nosound();
+sound(0);delay(20);nosound();sound(g*2);delay(200);nosound();sound(0);
+delay(50);nosound();
+
+printf("\n  He said there's a lot of girls I can do with");
+sound(b*2);delay(200);nosound();sound(0);delay(20);nosound();sound(g*2);
+delay(200);nosound();sound(0);delay(20);nosound();sound(g*2);delay(200);
+nosound();sound(0);delay(20);nosound();sound(g*2);delay(200);nosound();
+sound(0);delay(20);nosound();sound(d*2);delay(200);nosound();sound(0);
+delay(20);nosound();sound(d*2);delay(200);nosound();sound(0);delay(20);
+nosound();sound(d*2);delay(200);nosound();sound(0);delay(20);nosound();
+sound(e*2);delay(200);nosound();sound(0);delay(20);nosound();sound(d*2);
+delay(200);nosound();sound(0);delay(20);nosound();sound(c*2);delay(200);
+nosound();sound(0);delay(20);nosound();sound(d*2);delay(200);nosound();
+sound(0);delay(50);nosound();
+
+printf("\n  (But I can't without you)");
+sound(d*2);delay(200);nosound();sound(0);delay(20);nosound();sound(c*2);
+delay(200);nosound();sound(0);delay(20);nosound();sound(d*2);delay(200);
+nosound();sound(0);delay(20);nosound();sound(c*2);delay(200);nosound();
+sound(0);delay(20);nosound();sound(g*2);delay(200);nosound();sound(0);
+delay(50);nosound();
+
+printf("\n  I knew him forever in a minute");
+sound(d*2);delay(200);nosound();sound(0);delay(20);nosound();sound(b*2);
+delay(200);nosound();sound(0);delay(20);nosound();sound(g*2);delay(200);
+nosound();sound(0);delay(20);nosound();sound(g*2);delay(200);nosound();
+sound(0);delay(20);nosound();sound(g*2);delay(200);nosound();sound(0);
+delay(20);nosound();sound(g*2);delay(200);nosound();sound(0);delay(20);
+nosound();sound(a*2);delay(200);nosound();sound(0);delay(20);nosound();
+sound(b*2);delay(200);nosound();sound(0);delay(20);nosound();sound(a*2);
+delay(200);nosound();sound(0);delay(20);nosound();sound(g*2);delay(200);
+nosound();sound(0);delay(20);nosound();sound(a*2);delay(200);nosound();
+sound(0);delay(50);nosound();
+
+printf("\n  There summer night in June");
+sound(d*2);delay(200);nosound();sound(0);delay(20);nosound();sound(c*2);
+delay(200);nosound();sound(0);delay(20);nosound();sound(d*2);delay(200);
+nosound();sound(0);delay(20);nosound();sound(c*2);delay(200);nosound();
+sound(0);delay(20);nosound();sound(g*2);delay(200);nosound();sound(0);
+delay(50);nosound();
+
+printf("\n  And papa says he got malo in him");
+sound(b*2);delay(200);nosound();sound(0);delay(20);nosound();sound(g*2);
+delay(200);nosound();sound(0);delay(20);nosound();sound(g*2);delay(200);
+nosound();sound(0);delay(20);nosound();sound(g*2);delay(200);nosound();
+sound(0);delay(20);nosound();sound(d*2);delay(200);nosound();sound(0);
+delay(20);nosound();sound(d*2);delay(200);nosound();sound(0);delay(20);
+nosound();sound(e*2);delay(200);nosound();sound(0);delay(20);nosound();
+sound(d*2);delay(200);nosound();sound(0);delay(20);nosound();sound(c*2);
+delay(200);nosound();sound(0);delay(20);nosound();sound(d*2);delay(200);
+nosound();sound(0);delay(50);nosound();
+
+printf("\n  He got me feeling like");
+sound(d*2);delay(200);nosound();sound(0);delay(20);nosound();sound(c*2);
+delay(200);nosound();sound(0);delay(20);nosound();sound(d*2);delay(200);
+nosound();sound(0);delay(20);nosound();sound(c*2);delay(200);nosound();
+sound(0);delay(20);nosound();sound(g*2);delay(200);nosound();sound(0);
+delay(50);nosound();
+
+printf("\n  Oooh-oooh-ooh");
+sound(f);delay(200);nosound();sound(0);delay(20);nosound();sound(g);
+delay(200);nosound();sound(0);delay(20);nosound();sound(f);delay(200);
+nosound();sound(0);delay(20);nosound();sound(g);delay(200);nosound();sound(0);
+delay(20);nosound();sound(c);delay(200);nosound();sound(0);delay(20);
+nosound();sound(d);delay(200);nosound();sound(0);delay(20);nosound();
+sound(c);delay(200);nosound();sound(0);delay(20);nosound();sound(b);
+delay(200);nosound();sound(0);delay(20);nosound();sound(c);delay(200);
+nosound();sound(0);delay(20);nosound();sound(a);delay(200);nosound();
+sound(0);delay(50);nosound();
+
+printf("\n  I knew it when I met him");
+sound(f);delay(200);nosound();sound(0);delay(20);nosound();sound(f);
+delay(200);nosound();sound(0);delay(20);nosound();sound(f);delay(200);
+nosound();sound(0);delay(20);nosound();sound(f);delay(200);nosound();
+sound(0);delay(20);nosound();sound(f);delay(200);nosound();sound(0);delay(20);
+nosound();sound(g);delay(200);nosound();sound(0);delay(20);nosound();
+sound(d);delay(200);nosound();sound(0);delay(50);nosound();
+
+printf("\n  I loved when I left him");
+sound(d);delay(200);nosound();sound(0);delay(20);nosound();sound(c);
+delay(200);nosound();sound(0);delay(20);nosound();sound(c);delay(200);
+nosound();sound(0);delay(20);nosound();sound(c);delay(200);nosound();
+sound(0);delay(20);nosound();sound(b);delay(200);nosound();sound(0);
+delay(20);nosound();sound(d);delay(200);nosound();sound(0);delay(20);
+nosound();sound(a);delay(200);nosound();sound(0);delay(50);nosound();
+
+printf("\n  Got me feeling like");
+sound(g);delay(200);nosound();sound(0);delay(20);nosound();sound(g);
+delay(200);nosound();sound(0);delay(20);nosound();sound(g);delay(200);
+nosound();sound(0);delay(20);nosound();sound(g);delay(200);nosound();
+sound(0);delay(20);nosound();sound(g);delay(200);nosound();sound(0);
+delay(50);nosound();
+
+printf("\n  Oooh-oooh-ooh");
+sound(f);delay(200);nosound();sound(0);delay(20);nosound();sound(g);
+delay(200);nosound();sound(0);delay(20);nosound();sound(f);delay(200);
+nosound();sound(0);delay(20);nosound();sound(g);delay(200);nosound();
+sound(0);delay(20);nosound();sound(c);delay(200);nosound();sound(0);
+delay(20);nosound();sound(d);delay(200);nosound();sound(0);delay(20);
+nosound();sound(c);delay(200);nosound();sound(0);delay(20);nosound();
+sound(b);delay(200);nosound();sound(0);delay(20);nosound();sound(c);
+delay(200);nosound();sound(0);delay(20);nosound();sound(a);delay(200);
+nosound();sound(0);delay(50);nosound();
+
+printf("\n  And then I had to tell him");
+sound(f);delay(200);nosound();sound(0);delay(20);nosound();sound(f);
+delay(200);nosound();sound(0);delay(20);nosound();sound(f);delay(200);
+nosound();sound(0);delay(20);nosound();sound(f);delay(200);nosound();
+sound(0);delay(20);nosound();sound(f);delay(200);nosound();sound(0);
+delay(20);nosound();sound(g);delay(200);nosound();sound(0);delay(20);
+nosound();sound(d);delay(200);nosound();sound(0);delay(50);nosound();
+
+printf("\n  I had to go , na-na-na-na");
+sound(g);delay(200);nosound();sound(0);delay(20);nosound();sound(d);
+delay(200);nosound();sound(0);delay(20);nosound();sound(d);delay(200);
+nosound();sound(0);delay(20);nosound();sound(b);delay(200);nosound();
+sound(0);delay(20);nosound();sound(a);delay(200);nosound();sound(0);
+delay(20);nosound();sound(g);delay(200);nosound();sound(0);delay(20);
+nosound();sound(g);delay(200);nosound();sound(0);delay(20);nosound();
+sound(g);delay(200);nosound();sound(0);delay(20);nosound();sound(g);
+delay(200);nosound();sound(0);delay(20);nosound();sound(g);delay(200);
+nosound();sound(0);delay(50);nosound();
+
+/**/
+printf("\n  Havana , ooh-na-na");
+sound(b);delay(250);nosound();sound(0);delay(20);nosound();sound(d);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(b);delay(250);nosound();
+sound(0);delay(20);nosound();sound(b);delay(250);nosound();sound(0);
+delay(20);nosound();sound(g);delay(250);nosound();sound(0);delay(20);
+nosound();
+
+printf("\n  Half of my heart is in Havana , ooh-na-na");
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(c);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(e);delay(250);nosound();
+sound(0);delay(20);nosound();sound(d);delay(250);nosound();sound(0);
+delay(20);nosound();sound(c);delay(250);nosound();sound(0);delay(20);
+nosound();sound(b);delay(250);nosound();sound(0);delay(20);nosound();
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(d);
+delay(250);nosound();sound(0);delay(20);nosound();sound(b);delay(250);
+nosound();sound(0);delay(20);nosound();sound(b);delay(250);
+nosound();sound(0);delay(20);nosound();sound(g);delay(250);nosound();
+sound(0);delay(20);nosound();
+
+printf(" \n  He took me back to East Atlanta , na-na-na");
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(c);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(e);delay(250);nosound();
+sound(0);delay(20);nosound();sound(d);delay(250);nosound();sound(0);
+delay(20);nosound();sound(c);delay(250);nosound();sound(0);delay(20);
+nosound();sound(b);delay(250);nosound();sound(0);delay(20);nosound();
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(d);
+delay(250);nosound();sound(0);delay(20);nosound();sound(b);delay(250);
+nosound();sound(0);delay(20);nosound();sound(b);delay(250);nosound();
+sound(0);delay(20);nosound();sound(g);delay(250);nosound();sound(0);
+delay(20);nosound();
+
+printf("\n  All but my heart is in Havana");
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(c);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(e);delay(250);nosound();
+sound(0);delay(20);nosound();sound(d);delay(250);nosound();sound(0);
+delay(20);nosound();sound(c);delay(250);nosound();sound(0);delay(20);
+nosound();sound(b);delay(250);nosound();sound(0);delay(20);nosound();
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(b);
+delay(250);nosound();sound(0);delay(20);nosound();
+
+printf("\n  There is something about his manners");
+sound(d);delay(250);nosound();sound(0);delay(20);nosound();sound(c);
+delay(250);nosound();sound(0);delay(20);nosound();sound(d);delay(250);
+nosound();sound(0);delay(20);nosound();sound(c);delay(250);nosound();sound(0);
+delay(20);nosound();sound(b);delay(250);nosound();sound(0);delay(20);
+nosound();sound(d);delay(250);nosound();sound(0);delay(20);nosound();
+sound(a);delay(250);nosound();sound(0);delay(20);nosound();
+
+printf("\n  Havana , ooh-na");
+sound(b);delay(250);nosound();sound(0);delay(20);nosound();sound(a);delay(250);
+nosound();sound(0);delay(20);nosound();sound(b);delay(250);nosound();sound(0);
+delay(20);nosound();sound(a);delay(250);nosound();sound(0);delay(20);
+nosound();sound(b);delay(250);nosound();sound(0);delay(20);nosound();
+
+printf("\n  \n                        \n  Like , share and subscribe for more stuff \n  Visit our channel to find fabulous videos \n  Jay Hind.\n                        ");
+nosound();
+getch();
+}
