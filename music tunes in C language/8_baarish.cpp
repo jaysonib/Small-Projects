e4bfc67db5a1a71dc623eb3d95f14bba5c2de286
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 18:28:24 -0400

    Create 8_baarish.cpp

---
+++ music tunes in C language/8_baarish.cpp
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+void main()
+{
+clrscr();
+int a=440,b=493,c=523,d=587,e=659,f=698,g=783;
+printf(" Half girlfriend - \" Barish \" -music tune in c language\n The lyrics are as below : \n                                   \n  Chehre mein tere \n  Khud ko mein dhoondhun \n  Aamkho ke darmiyaan \n  Tu abb hain iss tarah \n  khwabbon ko bhi jagah na mile \n  Yeh mausam ki baarish \n  Yeh baarish ka pani\n  Yeh pani ki boondein \n  tujhe hi to dhoondhe \n  Yeh milne ki khwaahis\n  Yeh khwaahis purani \n  Ho puri tujhi se \n  Meri yeh kahaani\n                                   \n YOU CAN TRY THIS PROGRAM IN TURBO C++\n BY :- JBS\n                                   ");
+// Chehre mein tere
+sound(g);delay(300);nosound();sound(f);delay(300);nosound();sound(e);
+delay(300);nosound();sound(d);delay(300);nosound();sound(d);delay(300);
+nosound();sound(e);delay(300);nosound();sound(d);delay(300);nosound();
+sound(c);delay(300);nosound();
+// Khud ko mein dhoondhun
+sound(g);delay(300);nosound();sound(f);delay(300);nosound();sound(e);
+delay(300);nosound();sound(d);delay(300);nosound();sound(d);delay(300);
+nosound();sound(e);delay(300);nosound();sound(f);delay(500);nosound();
+sound(e);delay(300);nosound();sound(d);delay(300);nosound();sound(e);
+delay(300);nosound();sound(d);delay(300);nosound();sound(c);delay(300);
+nosound();
+// Aamkho ke darmiyaan
+sound(e);delay(300);nosound();sound(d);delay(300);nosound();sound(0);
+delay(20);nosound();sound(d);delay(300);nosound();sound(0);delay(20);
+nosound();sound(d);delay(300);nosound();sound(0);delay(20);nosound();
+sound(d);delay(300);nosound();sound(e);delay(300);nosound();sound(c);
+delay(300);nosound();sound(b);delay(300);nosound();
+// Tu abb hain iss tarah
+sound(e);delay(300);nosound();sound(d);delay(300);nosound();sound(0);
+delay(20);nosound();sound(d);delay(300);nosound();sound(0);delay(20);
+nosound();sound(d);delay(300);nosound();sound(0);delay(20);nosound();
+sound(d);delay(300);nosound();sound(e);delay(300);nosound();sound(c);
+delay(300);nosound();sound(b);delay(300);nosound();
+// khwabbon ko bhi jagah na mile
+sound(e);delay(300);nosound();sound(d);delay(300);nosound();sound(0);
+delay(20);nosound();sound(d);delay(300);nosound();sound(0);delay(20);
+nosound();sound(d);delay(300);nosound();sound(0);delay(20);nosound();
+sound(d);delay(300);nosound();sound(e);delay(300);nosound();sound(c);
+delay(300);nosound();sound(b);delay(200);nosound();sound(0);delay(20);
+nosound();sound(b);delay(200);nosound();sound(0);delay(20);nosound();
+sound(b);delay(200);nosound();sound(c);delay(300);nosound();sound(b);
+delay(300);nosound();sound(a);delay(300);nosound();
+// Yeh mausam ki baarish
+sound(c);delay(350);nosound();sound(0);delay(20);nosound();sound(c);
+delay(350);nosound();sound(d);delay(350);nosound();sound(e);delay(350);
+nosound();sound(b);delay(350);nosound();sound(c);delay(350);nosound();
+sound(0);delay(100);nosound();
+// Yeh baarish ka pani
+sound(c);delay(350);nosound();sound(0);delay(20);nosound();sound(c);
+delay(350);nosound();sound(d);delay(350);nosound();sound(e);delay(350);
+nosound();sound(b);delay(350);nosound();sound(c);delay(350);nosound();
+// Yeh pani ki boondein
+sound(b);delay(300);nosound();sound(0);delay(30);nosound();sound(b);
+delay(350);nosound();sound(c);delay(350);nosound();sound(d);delay(350);
+nosound();sound(b);delay(350);nosound();sound(c);delay(350);nosound();
+sound(0);delay(70);nosound();
+// tujhe hi to dhoondhe
+sound(b);delay(350);nosound();sound(0);delay(30);nosound();sound(b);
+delay(350);nosound();sound(c);delay(350);nosound();sound(d);delay(350);
+nosound();sound(b);delay(350);nosound();sound(c);delay(350);nosound();
+// Yeh milne ki khwaahis
+sound(c);delay(350);nosound();sound(0);delay(20);nosound();sound(c);
+delay(350);nosound();sound(d);delay(350);nosound();sound(e);delay(350);
+nosound();sound(b);delay(350);nosound();sound(c);delay(350);nosound();
+sound(0);delay(100);nosound();
+// Yeh khwaahis purani
+sound(c);delay(350);nosound();sound(0);delay(20);nosound();sound(c);
+delay(350);nosound();sound(d);delay(350);nosound();sound(e);delay(350);
+nosound();sound(b);delay(350);nosound();sound(c);delay(350);nosound();
+// Ho puri tujhi se
+sound(b);delay(300);nosound();sound(0);delay(30);nosound();sound(b);
+delay(350);nosound();sound(c);delay(350);nosound();sound(d);delay(350);
+nosound();sound(b);delay(350);nosound();sound(c);delay(350);nosound();
+sound(0);delay(70);nosound();
+// Meri yeh kahaani
+sound(b);delay(300);nosound();sound(0);delay(30);nosound();sound(b);
+delay(300);nosound();sound(c);delay(300);nosound();sound(g);delay(300);
+nosound();sound(f);delay(300);nosound();sound(e);delay(300);
+nosound();sound(0);delay(30);nosound();sound(e);delay(600);nosound();
+getch();}
