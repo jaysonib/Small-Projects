966ea09f0e9d3131c7b5f139d1860c14c2efd336
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 20:20:35 -0400

    Create 8_birthday_cake.cpp

---
+++ Obfuscated_code/8_birthday_cake.cpp
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include<dos.h>
+#define s sound
+#define t delay
+void main(){clrscr();int gd=DETECT,gm;char z[50]; int count;
+initgraph(&gd,&gm,"C:\\turboc3\\bgi");printf("print no. of "
+"seconds left"); scanf("          %d",&count);settextstyle(7
+,HORIZ_DIR,5);for(int                  i=count;i>0*0;i=i-1){
+sprintf(z," Starts"                      " in %d seconds",i)
+;outtextxy(10,210                          ,z);delay( 1000);
+cleardevice();}                              settextstyle(3,
+HORIZ_DIR, 4);                                setcolor(WHITE
+);outtextxy(                                    30,200,"MAD"
+"E BY :- P"                                      "ROGRAMMIN"
+"G KNOWLE"                                        "DGE");t(0
++25*20) ;                                          setcolor(
+GREEN) ;                                           outtextxy
+(15+15/*                *          *                */,300 ,
+"MADE "                                         	     "BY :-"
+" PRO"                       ""                       "GRAM"
+"MI"/*                  *          *                  */"NG"
+" "/*                   *    **   **                   */"K"
+"N"/*                   *    **   **                   */"O"
+""/*                    *    **   **                    */""
+"WL"                    ""   ""   ""                    "ED"
+"G"                   "E");t(5*010+440                   +10
+);;                  setcolor(RED);char                  abc
+,l;                 outtextxy(30,100,"M"                 "A"
+"D"                 "E BY :- PROGRAMMIN"                 "G"
+""                   " KNOWLEDGE");t(0+                   10
+*5                   );int j=0,a=391,b=                   40
++1                  +399,c=493,d=523,e=8                  +1
++1                  +577,f=659,g=698,h=1                  +1
++1                  +780;s(a);t(250);s(j                  );
+t(                   10+10);s(a);t(250)                   ;s
+(b               +1);t(25*10);s(a);t(5*5*10               );
+s(              d);t(5*5*5*2);s(c);t(5*2*50)              ;s
+(j             +0);t(3*5*2*5*2);s(a);t(5*5*10             );
+s(             j-j+j);t(2*5*2);s(a+a-a);t(250             );
+s(b            );t(250);s(a);t(50+50+50+50+50            );s
+(e)            ;t(250);s(d);t(500);s(a);t(250            );s
+(a)             ;t(50*50/10);s(h);t(250);s(f             );t
+(50            +200);s(d);t(5*5*10);s(c);t(50            +50
++150           );s(b);t(20*20);s(g);t(25*10);           s(g)
+;t(0           +250);s(f);t(50*50/10);s(d);t(           250)
+;s(e)          ;t(5*5*10);s(d);t(3*2*100);s(a          );t(0
++250)          ;s(a-400+9);t(20);s(a); t(5*10          +200)
+;s(b);         t(50+50+50+100);s(a);t(250+1);         s(d);t
+(250);      s(c);t(5*2*2*5+400);s(+a-a);t(300         );s(a)
+;t(250)    /*********celyui rqiut ed QY*********/    ;s(j);t
+(30-10);   /*******fhewhqccydw adembutwu********/   s(a);t(1
++249);s(                                            b);t(250
+);s(a);t(                           	           250);s(e)
+;t(1<<8)/*             HAPPY BIRTHDAY             */;s(1000-
+b);t(500)/*             CODE IN C BY             */;s(a);t(0
++250);s(a)/*       PROGRAMMING KNOWLEDGE        */;t(250);s(
+h);t(250);s(f)                                ;t(250);s(d);t
+(250);s(c);t(25                              *10);s(11*4*10)
+;t(4*100);s(g);t(                          500);s(f);t(250);
+s(d);t(250);s(e);t(                      250);s(d);t(30*20);
+nosound();cleardevice                  ();settextstyle(0007,
+HORIZ_DIR,5);outtextxy(10          ,210,"LIKE, SHARE AND SU"
+"BSCRIBE");delay(1000);outtextxy(10,250,"PROGRAMMING KNOWLE"
+"GE!!!!!!!!    ");delay(5*2*2*5*5*2+1+6+2+2+0+2+1);getch();}
