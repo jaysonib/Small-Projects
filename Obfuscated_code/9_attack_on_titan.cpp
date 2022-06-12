583df60f05fd5260a822adcd5974189b101b1861
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 20:21:41 -0400

    Create 9_attack_on_titan.cpp

---
+++ Obfuscated_code/9_attack_on_titan.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>/*made by programming knowledge*/
+int main(){int key=2+2;   int c,m,i,j;char cc;for(
+i=key-key,m=1<<5|1;cc      ="ALevi+=NnAarUt0C+=X(\
+)=@nAraSlsFJAW()=aa"        "A=ERENHggVkf=afA12Q$\
+dIeeVef=aaAInQuiIt"         "rVgr=ntA@tRuTH{}V()=\
+YeAgER+=Ho0VeR=++A"         "+=SwIFtsWan=atAckSom\
+EPYXiS=faAddSejEanX"        "32=asAhiQdsHERWIN=12\
+AiIP+=JayV()=ehAiIP"        "ZeKePURE=PlAn=P()B12\
+IamATITAN=meAawOgdCd"      "yBtoBioA4fB65B6CArtT=\
+x=asAhiOldCarATTACKA"     "TTACKATTACKATTACKATTAC\
+KT@g=haAllOf+C=wInGA"     "daTfe=efAbAOYaChiAvvBb\
+hC#rBbEAstAtiTan=b"        "eAe{OwlD_oB()AttAckAs\
+eBmfBerTol=tgAgaN"          "aeFhyAttACKATtAcKAs#\
+D{}Sma=lLAKeNnYGz"           "fCafEfeSht=scAHANGE\
+PooR()=++AKeNNy"/*  !!!!!!!!! */"PpPReD=feAarMinP\
+saSor=I+AARMIN"/*   !!  !  !! */"QqQReD=++AF@LCoR\
+feRar=i;AwaLL="/*   ! ! ! ! ! */"QqQSsS=90AarLerQ\
+kuShi=naA+=Kgf"/*   !!!!!!!!! */"SsSRrR=grAnhKnhT\
+AJQjy=hXAb;Jay"/*   !  !!!  ! */"VvVPpP=WwAlLJa@W\
+JsOnI=awAf;Iam"/*    !! ! !!  */"Zo0MiK=ASA+=HamM\
+erAarMiNM++=a"/*      ! ! !    */"@AanGleN()Bo0M=\
+@LiE=@fAlLFly"/*       !!!     */"OnlB+=MARLEY=##\
+A,ZEKEP()BAnNie"                "Kak=shAELDiaC=-A\
+juMmICr0PxcHre="                "rtAOTCRtCT0BioHi\
+nAtaD()CMaDaRA"                "oiKawHin=atAerBdf\
+ChtCfeGgaBi=D+"                 "=ColD++AsYMirFri\
+=tzAtTAckC++C"                  "ajHfdBhIDe=DaiCh\
+=BFeMaLETI=ta"                 "D,nDkjGfgChIDegDG\
+@C12BiINo0D()"                 "=sdCeeDsdHfgChIDe\
+ACkeE=OBitO="                   "fBfB=@fBfBFsuGaa\
+C()EKiBa=FeE"                    "B+=JayB()C=GABI\
+=GaAraF()Hi"                      "@CcODhiDanE()C\
+@LIfeC()C++"                       "=a*AllFmiGhtD\
+DoDieEndD!"                          "@DuCHihD+=B\
+gO=UkAI"/*             "             */"oG+=Fo0D=\
+qDdiEenD"              ""             "olD()HalF=\
+T=aNAka"               ""             "Go=Fo0D+=D\
+grEedD"                ""              "hIDaNHalF\
+()="/*   *             """                */"@aAa\
+oG()"   /*        *    """    "           */"E+=E\
+reD"   ","       /*    "E"    ;             */",E\
+re"   "D"        ""    "E,"   ""             "End\
+F"   "au"       "G"    "au"   ""              "=!\
+"   ";A"        "="    "a"     ""               ""
+   "Ho0D"       ";"     ""      ""          /*   1
+  */",F"        "="    "-C"     "+"         "D"   
+ "FAlCO"       "hE"    "loF"    "og"        "Go"  
+"B=aSAhi"      "He"    "cCc"    "OG"        "r0Ck\
+#FalCo+G"      "HE"    "DoC"    "Go"        "<=>;\
+AiIRo0C+"     "aFa"    "cCc"    "oGo"      "!C!!H\
+hi=naAtaR"    "GaBI"   "=H()"   "BiG"      "HumAN"
+"MIKA=SAA"   "LEvi;"   [(!(cc   -61)?(m    =1<<5+0
+,printf("\n")):(m^=1   )),i+=   (1+1+1)   ];){for(
+j=0;(cc-61)&&(j++-cc  -(cc<64?5:(cc>90?- 70:-64)))
+;printf("%c",m));}printf("\nSUBSCRIBE");return 0;}
+/************************************************/
+/*                ATTACK ON TITAN               */
+/*                      BY                      */
+/*            PROGRAMMING KNOWLEDGE             */
+/************************************************/
