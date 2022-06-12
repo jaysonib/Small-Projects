3b91a32639489b4cfadd219b06f35b72d3b243ce
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-06-11 20:31:00 -0400

    Create 26_tribute_to_lata_mangeshkar.cpp

---
+++ music tunes in C language/26_tribute_to_lata_mangeshkar.cpp
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+void main(){
+clrscr();
+
+int a = 590, b = 660, c = 740, d = 830, e = 880, f = 1110, g = 1175, h = 1320;
+
+printf("\n Tribute to Queen of Melody - LATA MANGESHKAR;");
+printf("\n LYRICS: ");
+printf("\n Naam gum jaayega ");
+printf("\n Chehra ye badal jayega ");
+printf("\n Meri aawaaz hi pehchaan hai, gar yaad rahe..");
+sound(e);delay(300);nosound();sound(0);delay(50);nosound();sound(e);
+delay(300);nosound();sound(d);delay(300);nosound();sound(c);delay(300);
+nosound();sound(b);delay(300);nosound();sound(d);delay(300);nosound();
+sound(c);delay(300);nosound();sound(0);delay(50);nosound();sound(c);
+delay(500);nosound();sound(e);delay(300);nosound();sound(c);delay(300);
+nosound();sound(b);delay(300);nosound();sound(a);delay(300);nosound();
+sound(e);delay(300);nosound();sound(0);delay(50);nosound();sound(e);
+delay(300);nosound();sound(0);delay(50);nosound();sound(e);delay(300);
+nosound();sound(d);delay(300);nosound();sound(c);delay(300);nosound();
+sound(b);delay(300);nosound();sound(d);delay(300);nosound();sound(c);
+delay(300);nosound();sound(0);delay(50);nosound();sound(c);delay(300);
+nosound();sound(0);delay(100);nosound();sound(c);delay(300);nosound();
+sound(e);delay(300);nosound();sound(f);delay(300);nosound();sound(g);
+delay(300);nosound();sound(h);delay(300);nosound();sound(g);delay(300);
+nosound();sound(f);delay(300);nosound();sound(e);delay(300);nosound();
+sound(c);delay(300);nosound();sound(0);delay(50);nosound();sound(c);
+delay(300);nosound();sound(e);delay(300);nosound();sound(c);delay(300);
+nosound();sound(b);delay(300);nosound();sound(a);delay(300);nosound();
+sound(0);delay(50);nosound();sound(a);delay(300);nosound();sound(c);
+delay(300);nosound();sound(0);delay(50);nosound();sound(c);delay(300);
+nosound();sound(b);delay(500);nosound();sound(0);delay(50);nosound();
+sound(e);delay(300);nosound();sound(0);delay(50);nosound();sound(e);
+delay(300);nosound();sound(b);delay(300);nosound();sound(c);delay(300);
+nosound();sound(b);delay(300);nosound();sound(a);delay(300);nosound();
+sound(e);delay(300);nosound();sound(0);delay(50);nosound();sound(e);
+delay(300);nosound();sound(b);delay(300);nosound();sound(c);delay(300);
+nosound();sound(b);delay(300);nosound();sound(a);delay(500);nosound();
+sound(0);delay(1000);nosound();sound(d);delay(300);nosound();sound(e);
+delay(300);nosound();sound(0);delay(50);nosound();sound(e);delay(300);
+nosound();sound(d);delay(300);nosound();sound(c);delay(300);nosound();
+sound(b);delay(300);nosound();sound(d);delay(300);nosound();sound(c);
+delay(300);nosound();sound(0);delay(50);nosound();sound(c);delay(300);
+nosound();sound(e);delay(300);nosound();sound(c);delay(300);nosound();
+sound(b);delay(300);nosound();sound(a);delay(300);nosound();sound(e);
+delay(300);nosound();sound(0);delay(50);nosound();sound(e);delay(300);
+nosound();sound(0);delay(300);nosound();sound(e);delay(300);nosound();
+sound(d);delay(300);nosound();sound(c);delay(300);nosound();sound(b);
+delay(300);nosound();sound(d);delay(300);nosound();sound(c);delay(300);
+nosound();sound(0);delay(50);nosound();sound(c);delay(300);nosound();
+sound(c);delay(300);nosound();sound(e);delay(300);nosound();sound(f);
+delay(300);nosound();sound(g);delay(300);nosound();sound(h);delay(300);
+nosound();sound(g);delay(300);nosound();sound(f);delay(300);nosound();
+sound(e);delay(300);nosound();sound(c);delay(300);nosound();sound(0);
+delay(50);nosound();sound(c);delay(300);nosound();sound(e);delay(300);
+nosound();sound(c);delay(300);nosound();sound(b);delay(300);nosound();
+sound(a);delay(300);nosound();sound(0);delay(50);nosound();sound(a);
+delay(300);nosound();sound(c);delay(300);nosound();sound(0);delay(50);
+nosound();sound(c);delay(300);nosound();sound(b);delay(800);nosound();
+getch();
+}
